d2269ef30c6e54171f93dabc102139b7860c3293
GManentis <manentis.gerasimos@outlook.com>
2023-06-25 20:54:16 +0300

    optimized controller

--- src/Controller/MathController.php
+++ src/Controller/MathController.php
@@ -9,10 +9,12 @@ use App\Service\Maths;
 
 class MathController extends AbstractController
 {
+    const operations = ["add", "subtract", "multiply", "divide"];
+
     #[Route('/{operation}/{firstNumber}/{secondNumber}', name: 'app_math', methods: ['GET'])]
     public function index( $operation, $firstNumber, $secondNumber, Maths $mathObject ): Response
     {
-        if( !in_array($operation, ["add", "subtract", "multiply", "divide"]) )    return new Response("The selected opearation is not available", 404);
+        if( !in_array($operation, self::operations) ) return new Response("The selected operation is not available", 404);
         if( !is_numeric($firstNumber) || !is_numeric($secondNumber) )   return new Response("Both params must be numbers", 422);
         return new Response($mathObject->{$operation}($firstNumber, $secondNumber));
     }
